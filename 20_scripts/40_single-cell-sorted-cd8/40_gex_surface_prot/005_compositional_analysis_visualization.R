c621152d0debb52839f95a4dc0a12a11ee6c8ca3
Erika Kvalem <erika.kvalem@gmail.com>
2024-08-02 15:20:13 +0200

    compositional analysis for Ifn

--- 20_scripts/40_single-cell-sorted-cd8/40_gex_surface_prot/005_compositional_analysis_visualization.R
+++ 20_scripts/40_single-cell-sorted-cd8/40_gex_surface_prot/005_compositional_analysis_visualization.R
@@ -1,72 +1,138 @@
+############ Compositional analysis for TIL Honda project 
+
+plot_df_inf = read_csv("plot_df_INF.csv")
+
+frac_by_condition_inf <- read_csv("frac_by_condition_INF.csv")
+
+plot_df= read_csv("plot_df.csv")
+colnames(plot_df)[colnames(plot_df) == 'Cell type'] <- 'cell_type'
+
+frac_by_condition <- read_csv("frac_by_condition.csv")
 
 
-
-
-df_num = read_csv("df_num.csv")
-df_num  = as.data.frame(df_num)
-df_num$log_cf = log(df_num$cell_fraction)
-
-
-## ALL 
-p0_results = compare_means(cell_fraction ~ condition, data = df_num, method = "wilcox.test")
-
-my_comparisons <- list( c("10mix", "11mix"), c("11mix", "GF"), c("10mix", "GF") )
-
-# Is there any difference between the conditions for cell_fraction 
-p0 = ggboxplot(df_num, x = "condition", y = "cell_fraction",color="condition",
-                palette = "jco", add = "jitter")  + 
-  stat_compare_means(comparisons = my_comparisons, label="p.signif", method="wilcox.test", ref.group = "GF")
-
-p0
-
-
-
-# Is there any difference between groups in cell_fraction/ cell_count 
-p1_results = compare_means(cell_fraction ~ cell_type,  data = df_num,
-              ref.group = ".all.", method = "t.test")
-# Calculate the maximum value for cell_fraction
-max_cell_fraction <- max(df_num$cell_fraction, na.rm = TRUE)
-
-p1= ggboxplot(df_num, x = "cell_type", y = "cell_fraction", color = "cell_type", 
-          add = "jitter", legend = "none") +
-  rotate_x_text(angle = 45)+
-  geom_hline(yintercept = mean(df_num$cell_fraction), linetype = 2)+ # Add horizontal line at base mean
- # stat_compare_means(method = "anova", label.y = 1600)+        # Add global annova p-value
-  stat_compare_means(label = "p.signif", method = "t.test",
-                     ref.group = ".all.", hide.ns = TRUE)   +
-  ylim(0, max_cell_fraction * 1.1)  # Set y-axis limits
-
-p1
-#Can conclude that cell fraction is significantly increased in SLCE_Effector group and
-#it’s significantly decreased in MPEC_Effector, SLEC_Terminal, MPEC_intermediate & SLEC_Plastic
-
-## Grouping variables  
+###################### SCCODA Cell proportion for 10mix, 11mix, GF 
 
 desired_order <- c("MPEC_Progenitor", "MPEC_Intermediate", "MPEC_Effector",
                    "SLEC_Plastic", "SLEC_Progenitor", "SLEC_Intermediate",
                    "SLEC_Ifn", "SLEC_Effector", "SLEC_Terminal")
 
 # Set the cell_type column to a factor with the specified order
-df_num$cell_type <- factor(df_num$cell_type, levels = desired_order)
-
-# Remove rows where condition is "GF"
-#df_num<- df_num[df_num$condition != "GF", ]
-
-
-p2_results = compare_means(cell_fraction ~ condition, data = df_num, 
-              group.by = "cell_type")
-
+plot_df$"Cell type" <- factor(plot_df$"cell_type", levels = desired_order)
 # Customize facet labels
-facet_labels <- c("10mix", "11mix")
+facet_labels <- c("10mix", "11mix","GF")
 
-# Box plot facetted by "dose"
-p2 <- ggboxplot(df_num, x = "cell_type", y = "cell_fraction",
-               color = "cell_type", palette = "jco",
-               add = "jitter",
-               facet.by = "condition", short.panel.labs = FALSE)+  rotate_x_text(angle = 45) + 
-  facet_wrap(~ condition, labeller = as_labeller(setNames(facet_labels, unique(df_num$condition)))) +
-  theme(legend.position = "none") + # Remove legend
-  ylab("Cell fraction")  + # Set y-axis label
-xlab("Cell type")
+
+p1 <- ggboxplot(plot_df, x = "cell_type", y = "Proportion",
+                color = "cell_type", palette = "jco",
+                add = "jitter",
+                facet.by = "condition", short.panel.labs = FALSE) +
+  rotate_x_text(angle = 45) +
+  facet_wrap(~ condition, labeller = as_labeller(setNames(facet_labels, unique(plot_df$condition)))) +
+  theme(legend.position = "none", # Remove legend
+        axis.text.x = element_text(size = 8)) + # Set x-axis label size
+  ylab("Cell proportion") + # Set y-axis label
+  xlab("") # Set x-axis label
+p1
+
+ggsave("cell_proportion.png", p1, width =6, height = 4)
+
+######################### Number of cells 
+
+desired_order <- c("MPEC_Progenitor", "MPEC_Intermediate", "MPEC_Effector",
+                   "SLEC_Plastic", "SLEC_Progenitor", "SLEC_Intermediate",
+                   "SLEC_Ifn", "SLEC_Effector", "SLEC_Terminal")
+
+# Set the cell_type column to a factor with the specified order
+frac_by_condition$cell_type <- factor(frac_by_condition$cell_type, levels = desired_order)
+
+
+p2 <- ggbarplot(frac_by_condition, "cell_type", "n_cells",
+                fill = "cell_type", color = "cell_type",   palette = "jco") + rotate_x_text(angle = 45) +  xlab("") +   ylab("Number of cells") + # Set y-axis label
+  theme(legend.position = "none",  axis.title.x = element_text(size = 10))
 
 p2 
+
+ggsave("number_of_cells.png", p2, width =6, height = 4)
+
+######################### # Is there any difference between groups in cell_count 
+p3_results = compare_means(n_cells ~ cell_type,  data = frac_by_condition,
+                           ref.group = ".all.", method = "t.test")
+# Calculate the maximum value for cell_fraction
+max_cell_num <- max(frac_by_condition$n_cells, na.rm = TRUE)
+
+p3= ggboxplot(frac_by_condition, x = "cell_type", y = "n_cells", color = "cell_type", 
+              add = "jitter", legend = "none",  palette = "jco") +
+  rotate_x_text(angle = 45)+
+  geom_hline(yintercept = mean(frac_by_condition$n_cells), linetype = 2)+ # Add horizontal line at base mean
+  # stat_compare_means(method = "anova", label.y = 1600)+        # Add global annova p-value
+  stat_compare_means(label = "p.signif", method = "t.test",
+                     ref.group = ".all.", hide.ns = TRUE)   +
+  ylim(0, max_cell_num * 1.1)+   ylab("Number of cells") + xlab("") +  theme(legend.position = "none", # Remove legend
+                                                                             axis.title.x = element_text(size = 8)) # Set x-axis label size # Set y-axis limits
+
+p3
+
+ggsave("number_of_cells_baseline.png", p3, width =6, height = 4)
+
+##########################  Cell proportion Treated vs Untreated GF all cell types 
+
+filtered_df <- subset(plot_df, condition == "GF")
+
+desired_order <- c("MPEC_Progenitor", "MPEC_Intermediate", "MPEC_Effector",
+                   "SLEC_Plastic", "SLEC_Progenitor", "SLEC_Intermediate",
+                   "SLEC_Ifn", "SLEC_Effector", "SLEC_Terminal")
+
+# Set the cell_type column to a factor with the specified order
+filtered_df$cell_type <- factor(filtered_df$cell_type, levels = desired_order)
+
+p4_results = compare_means(Proportion ~ treatment, data = filtered_df, method = "wilcox.test")
+
+my_comparisons <- list( c("treated", "naive") )
+
+# Is there any difference between the conditions for cell_fraction 
+p4 = ggboxplot(filtered_df, x = "cell_type", y = "Proportion",color="treatment",
+               palette = "jco")  + 
+  stat_compare_means(comparisons = my_comparisons, label="p.signif", method="wilcox.test", ref.group = "naive")+
+  rotate_x_text(angle = 45) +   ylab("Cell proportion") +   xlab("")  +  theme(
+        axis.title.x = element_text(size = 7)) # Set x-axis label size # Set y-axis label
+
+
+p4
+ggsave("cell_proportion_treated_vs_untreated_GF.png", p4, width =6, height = 4)
+
+############# Cell type: SLEC_Ifn TREATED only 
+conflicts_prefer(rstatix::filter)
+
+filtered_df <- plot_df
+#filtered_df <- plot_df %>% filter(cell_type == "SLEC_Ifn")
+#filtered_df <- filtered_df %>% filter(treatment == "naive")
+p5_results = compare_means(Proportion ~ condition, data = filtered_df, method = "wilcox.test")
+
+my_comparisons <- list( c("10mix", "11mix"), c("11mix", "GF"), c("10mix", "GF") )
+#my_comparisons <- list( c("treated", "naive") )
+# Is there any difference between the conditions for cell_fraction 
+p5 = ggboxplot(filtered_df, x = "condition", y = "Proportion",color="condition",
+               palette = "jco", add = "jitter")  + 
+  stat_compare_means(comparisons = my_comparisons, label="p.signif", method="wilcox.test.test", ref.group = "GF")+
+  theme(legend.position = "none")+  # Remove legend
+  ylab("Cell proportion") + # Set y-axis label
+  xlab("Treatment") # Set x-axis label
+
+p5
+
+ggsave("SLEC_Ifn_treated.png", p5, width =4, height = 4)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
